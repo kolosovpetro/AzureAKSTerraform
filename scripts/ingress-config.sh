c0b22a10e55a8e27604682633fccc5cba25717de
kolosovpetro <kolosovp94@gmail.com>
2023-09-09 19:18:03 +0200

    cluster tested | test manifests

--- scripts/ingress-config.sh
+++ scripts/ingress-config.sh
@@ -26,7 +26,7 @@ helm upgrade ingress-nginx nginx-stable/nginx-ingress \
 
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm repo update
-helm upgrade my-ingress --install ingress-nginx --repo https://kubernetes.github.io/ingress-nginx \
+helm upgrade my-ingress --install ingress-nginx --repo https://kubernetes.github.io/ingress-nginx `
   --namespace ingress --set controller.service.externalTrafficPolicy=Local --create-namespace
 
 kubectl --namespace ingress get services my-ingress-ingress-nginx-controller
