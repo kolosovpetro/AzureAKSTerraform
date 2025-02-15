600f71bf3fce3f36c5a318866bf2e37711fce398
kolosovpetro <kolosovp94@gmail.com>
2025-02-15 03:09:07 +0100

    fix formatting

--- main.tf
+++ main.tf
@@ -6,12 +6,12 @@ resource "azurerm_resource_group" "public" {
 }
 
 module "aks" {
-  source                      = "./modules/aks"
-  aks_name                    = "aks-${var.prefix}"
-  default_node_pool_type      = "VirtualMachineScaleSets"
-  default_node_pool_vm_size   = "Standard_DS2_v2"
-  kubernetes_version          = var.kubernetes_version
-  resource_group_location     = azurerm_resource_group.public.location
-  resource_group_name         = azurerm_resource_group.public.name
-  system_node_count           = 3
+  source                    = "./modules/aks"
+  aks_name                  = "aks-${var.prefix}"
+  default_node_pool_type    = "VirtualMachineScaleSets"
+  default_node_pool_vm_size = "Standard_DS2_v2"
+  kubernetes_version        = var.kubernetes_version
+  resource_group_location   = azurerm_resource_group.public.location
+  resource_group_name       = azurerm_resource_group.public.name
+  system_node_count         = 3
 }
