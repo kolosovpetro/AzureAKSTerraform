bec0f81929030f82aca6369d4ecae78fcc3ce17e
kolosovpetro <kolosovp94@gmail.com>
2025-02-16 18:05:43 +0100

    update outputs

--- output.tf
+++ output.tf
@@ -17,3 +17,7 @@ output "connect_command" {
 output "grafana_endpoint" {
   value = length(module.managed_grafana) > 0 ? module.managed_grafana[0].grafana_endpoint : null
 }
+
+output "node_pool_list_command" {
+  value = "az aks nodepool list --resource-group ${azurerm_resource_group.public.name} --cluster-name ${module.aks.name}"
+}
